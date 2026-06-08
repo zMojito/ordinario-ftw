dfae653c19f8d4fa9e255598bd740901424f6f2a
zMojito <emilianohdzga@gmail.com>
2026-06-07 19:34:16 -0600

    Modificaciones pequeñas en index y agregamos la vista de la tabla de aliens

---
+++ js/aliens.js
@@ -0,0 +1,38 @@
+function cargar() {
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.onload = function () {
+        const xmlDoc = xhttp.responseXML;
+        if (xmlDoc) {
+            const listaAliens = xmlDoc.getElementsByTagName("alien");
+            mifuncion(listaAliens);
+        } else {
+            console.error("No se pudo cargar el archivo XML.");
+        }
+    };
+    xhttp.open("GET", "../xml/aliens.xml");
+    xhttp.send();
+}
+
+function mifuncion(aliens) {
+    let table = "<table>";
+    
+    for (let i = 0; i < aliens.length; i++) {
+        const tituloNode = aliens[i].getElementsByTagName("titulo")[0];
+        const descNode = aliens[i].getElementsByTagName("descripcion")[0];
+        const imgNode = aliens[i].getElementsByTagName("imagen")[0];
+        const titulo = tituloNode ? tituloNode.textContent : "";
+        const desc = descNode ? descNode.textContent : "";
+        const imgPath = imgNode ? imgNode.textContent : "";
+
+        table += "<tr>" +
+            "<td><img src='../" + imgPath + "' alt='" + titulo + "' style='max-width: 100px; display: block; margin: 0 auto;'></td>" +
+            "<td><strong>" + titulo + "</strong></td>" +
+            "<td>" + desc + "</td>" +
+            "</tr>";
+    }
+    table += "</table>";
+    document.getElementById("lista-aliens").innerHTML = table;
+}
+
+cargar();
